b0bad34ea281dbcee698fdcd9e7014f8e5537f99
Priyesh Agrawal <priyeshagrawal@Priyeshs-MacBook-Air.local>
2024-05-29 16:16:42 +0530

    Chapter 5: structuired code in different files, file name should same as component name, default export and named export, imports for type of export, react hooks, useState and useEffect, react component rerendering, reconciliation algorithm, virtual DOM, React fiber, Diff algorithm

---
+++ src/utils/constants.js
@@ -0,0 +1,3 @@
+export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+export const LOGO_URL = "https://cdn.dribbble.com/users/5960208/screenshots/16398589/i_will_2_modern_minimalist_logo_design_within_12_hrs__9__4x.jpg";
+
